14e437300be79ebfeb3243dcb7b0f306c0149f08
Alumno <alumno@academic.local>
2022-08-24 14:50:30 -0400

    NavBar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,8 @@
 import './App.css';
+import NavBar from "./NavBar"
 
 function App() {
-  return (
-    // NavBar
-    <div>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">Empanadas Don Dante</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarColor01">
-            <ul className="navbar-nav me-auto">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Inicio
-                  <span className="visually-hidden">(current)</span>
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Productos</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Sobre Nosotros</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contacto</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </div>
-  );
+  return ( NavBar );
 }
 
 export default App;
